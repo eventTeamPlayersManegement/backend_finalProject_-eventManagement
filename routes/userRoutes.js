6661522efac5fd9775e1a91f3a1f1faa497833d9
Marcelearn <jaworskimarcel@gmail.com>
2023-02-22 20:42:28 +0100

    ajv validate postuser

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import * as user from "../controllers/userController.js";
+import validate from "../middleware/validate.js"
+import { postUser } from "./user.schema.js"
 
 const userRoutes = Router();
 
-userRoutes.route("/").get(user.getAll).post(user.create);
+userRoutes.route("/").get(user.getAll).post(validate(postUser), user.create);
 
 export default userRoutes;
