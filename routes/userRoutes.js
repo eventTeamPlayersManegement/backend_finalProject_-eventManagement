5ca737d7a4e312a2321920b1b56a150def560d09
Elena Rotari <elena.rotari@icloud.com>
2023-02-23 16:44:20 +0100

    register path/ changed on signup because of routes on frontend

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ import { postUser } from "./user.schema.js";
 const userRoutes = Router();
 
 userRoutes.route("/").get(user.getAll);
-userRoutes.route("/register").post(validate(postUser), user.create);
+userRoutes.route("/signup").post(validate(postUser), user.create);
 userRoutes.route("/login").post(user.login);
 
 export default userRoutes;
