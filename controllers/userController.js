37454c10fa120fe6f2efb391fdedf3521dd6da06
Marcelearn <jaworskimarcel@gmail.com>
2023-02-23 12:06:06 +0100

    login verify token and cookie

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import * as User from "../models/User.js";
+import token from "../lib/token.js"
 
 export const getAll = async (req, res, next) => {
   try {
@@ -18,6 +19,33 @@ export const create = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+export const login = async (req, res, next) => {
+   
+  try {
+      const result = await User.getOne({email: req.body.email});
+      const passwordIsEqual = await bcrypt.compare(req.body.password, result.password);
+      if(!passwordIsEqual)return res.status(401).end();
+      if(passwordIsEqual){
+          const userToken = token.signToken({id: result._id})
+          const expDate = 1000 * 60 * 60 * 24
+          res.cookie("jwt", userToken, {
+              sameSite: "lax",
+              maxAge: expDate,
+              httpOnly: true
+          })
+          res.cookie("loggedIn", result._id.toString(), {
+              sameSite: "lax",
+              maxAge: expDate,
+              httpOnly: false
+          })
+
+          return res.status(201).json({message: "successfully logged in", id: result._id})
+      }
+  } catch(error) {
+      next(error);
+  };
+
 };
 export const getOne = async (req, res, next) => {
   try {
