5402f8616849d5114d9941c5ec35017f3796fc48
Elena Rotari <elena.rotari@icloud.com>
2023-02-24 12:20:32 +0100

    bug fix add await to bcrypt

--- middleware/hashPWD.js
+++ middleware/hashPWD.js
@@ -1,10 +1,10 @@
 import bcrypt from "bcrypt";
 export default async (req, res, next) => {
   try {
-    if (!req.body.password || req.body.email) {
+    if (!req.body.password || !req.body.email) {
       next({ aprooved: false, message: "no email od password provided" });
     } else {
-      const hashdPWD = bcrypt.hash(req.body.password, 10);
+      const hashdPWD = await bcrypt.hash(req.body.password, 10);
       req.body.password = hashdPWD;
       next();
     }
